451a33f59e020ff09fd86c1f351cb5f84faebd15
Sally-Kam <skam2000@gmail.com>
2023-05-07 17:03:14 -0400

    add navbar

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
